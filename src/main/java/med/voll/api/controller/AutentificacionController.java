bc5add0b09cd9d32b2ad99fbfa30f7f4f0390ff4
kevinLL22 <lopez.kevin.45678@gmail.com>
2023-05-05 15:01:56 -0500

    controller autentificacion, dto autenticar usuario, bean authentication manager

---
+++ src/main/java/med/voll/api/controller/AutentificacionController.java
@@ -0,0 +1,29 @@
+package med.voll.api.controller;
+
+import med.voll.api.domain.usuario.DatosAutenticarUsuarios;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/login")
+public class AutentificacionController {
+
+    private final AuthenticationManager authenticationManager;
+
+    public AutentificacionController(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+
+    @PostMapping
+    public ResponseEntity autenticarUsuario(DatosAutenticarUsuarios datosUsuario){
+        Authentication token = new UsernamePasswordAuthenticationToken(datosUsuario.login(),
+                datosUsuario.clave());
+        authenticationManager.authenticate(token);
+        return ResponseEntity.ok().build();
+    }
+}
---
+++ src/main/java/med/voll/api/domain/usuario/DatosAutenticarUsuarios.java
@@ -0,0 +1,4 @@
+package med.voll.api.domain.usuario;
+
+public record DatosAutenticarUsuarios(String login, String clave) {
+}
